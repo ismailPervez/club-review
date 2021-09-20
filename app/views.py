3d64f7ec28e694be5bdc618a6b44b7377ee45df2
ismailpervez <freelanceismail001@gmail.com>
2021-09-21 00:10:57 +0300

    made a create post route

--- app/views.py
+++ app/views.py
@@ -56,3 +56,35 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('home'))
+
+@app.route("/create", methods=["GET", "POST"])
+def create():
+    form = CreatePitchForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        content = form.content.data
+        # the tags need to be JSON serializable before we store them in the database
+        tags = form.tags.data.split(' ')
+        # initial upvotes and downvotes are 0
+        review = Review(
+            title=title,
+            content=content,
+            tags=str(tags),
+            upvotes=0,
+            downvotes=0,
+            user_id=current_user.id
+        )
+
+        db.session.add(review)
+        db.session.commit()
+
+        print('form submitted successfully!')
+
+        return redirect(url_for("account"))
+
+    else:
+        print('not validated')
+
+    # review = Review(, ,)
+
+    return render_template("createpost.html", form=form)
