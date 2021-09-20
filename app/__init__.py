e2591e892c4ec718e461e4956ab0e0bdd858699c
ismailpervez <freelanceismail001@gmail.com>
2021-09-20 23:43:22 +0300

    initialized database

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from app.config import config_options
 
 # app init / bootstrap init (bootstrap.init_app(app))
@@ -10,3 +11,6 @@ bootstrap = Bootstrap()
 
 # set configurations
 app.config.from_object(config_options['dev_config'])
+
+# init database
+db = SQLAlchemy(app)
