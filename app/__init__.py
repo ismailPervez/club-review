872da9800a39b2b909a79091654d96cd7950a14a
ismailpervez <freelanceismail001@gmail.com>
2021-09-20 23:41:58 +0300

    added a config file

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+from app.config import config_options
 
 # app init / bootstrap init (bootstrap.init_app(app))
 app = Flask(__name__)
 Bootstrap(app)
 bootstrap = Bootstrap()
+
+# set configurations
+app.config.from_object(config_options['dev_config'])
