e0cffa4d40cf06d8cc0bf91ab753dfa465c570a9
ismailpervez <freelanceismail001@gmail.com>
2021-09-21 00:06:31 +0300

    set up User class from models for flask_login

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from flask_login import UserMixin
 
 '''
 we have 2 model classes - User and Review
@@ -14,8 +15,28 @@ __tablename__ = 'comments'
 this table will have 2 foreign keys, one pointing to the user who created the comment
 and the other to the post which has been commented on
 '''
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
-class User(db.Model):
+'''
+the User class will also inherit from UserMixin class
+this class attaches 4 methods
+
+Flask-login requires a User model with the following properties:
+
+    has an is_authenticated() method that returns True if the user has provided
+    valid credentials
+    has an is_active() method that returns True if the user’s account is active
+    has an is_anonymous() method that returns True if the current user is an anonymous user
+    has a get_id() method which, given a User instance, returns the unique ID for that object
+
+UserMixin class provides the implementation of this properties.
+Its the reason you can call for example is_authenticated to check if login
+credentials provide is correct or not instead of having to write a method to do that yourself.
+
+'''
+class User(db.Model, UserMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), nullable=False)
